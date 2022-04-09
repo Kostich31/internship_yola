79b14dfefda45b48e747d98607df9df3ff1715de
Konstantin <k.v.kostinich@mail.ru>
2022-04-09 19:33:50 +0300

    Добавил комментарии

--- Task 1/checkDate.js
+++ Task 1/checkDate.js
@@ -1,9 +1,18 @@
 function checkDate(timestamp) {
-    const date = new Date(timestamp);
+    // timestamp в секундах, поэтому переводим в ms
+    const date = new Date(timestamp * 1000);
+    // Использую UTC для унификации времени в случае, если временная зона отличается
+    const day = date.getUTCDate();
+    const month = date.getUTCMonth();
+    const year = date.getUTCFullYear();
+    
     const currentDate = new Date(Date.now());
-
+    const currentDay = currentDate.getUTCDate();
+    const currentMonth = currentDate.getUTCMonth();
+    const currentYear = currentDate.getUTCFullYear();
+    // Перенёс вычисления сразу в return для лаконичности и краткости
     return {
-        isSameDate: date.getTime() === currentDate.getTime(),
-        dayPeriod: date.getHours() > 11 ? 'pm' : 'am',
+        isSameDate: year === currentYear && month === currentMonth && day === currentDay,
+        dayPeriod: date.getUTCHours() > 11 ? 'pm' : 'am'
     };
 }
