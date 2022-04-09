baf35f05aceb17c03ef957288a22230f264ab642
Konstantin <k.v.kostinich@mail.ru>
2022-04-09 19:12:40 +0300

    Задача №1

--- Task 1/checkDate.js
+++ Task 1/checkDate.js
@@ -1,28 +1,9 @@
 function checkDate(timestamp) {
-    var day = new Date(timestamp * 1000).getDate();
-    var month = new Date(timestamp * 1000).getMonth();
-    var year = new Date(timestamp * 1000).getFullYear();
-    var hour = new Date(timestamp * 1000).getHours();
-
-    const current_Date = new Date(Date.now());
-    const current_day = current_Date.getDate();
-    const current_month = current_Date.getMonth() + 1;
-    const currentYear = current_Date.getFullYear();
-
-    let isSameDate = false;
-
-    if (year == currentYear) {
-        if (month == current_month) {
-            if (day == current_day) {
-                isSameDate = true;
-            } else {
-                isSameDate = false;
-            }
-        }
-    }
+    const date = new Date(timestamp);
+    const currentDate = new Date(Date.now());
 
     return {
-        isSameDate: isSameDate,
-        dayPeriod: hour > 11 ? 'pm' : 'am'
-    }
+        isSameDate: date.getTime() === currentDate.getTime(),
+        dayPeriod: date.getHours() > 11 ? 'pm' : 'am',
+    };
 }
